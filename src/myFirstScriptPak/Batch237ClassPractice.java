1007ce62627112f9232befd8d203fddcee4a7836
zballouch <zballouch2017@gmail.com>
2019-12-17 08:58:53 -0500

    Complete the script for account create automation script.

---
+++ src/myFirstScriptPak/Batch237ClassPractice.java
@@ -0,0 +1,17 @@
+package myFirstScriptPak;
+
+public class Batch237ClassPractice {
+
+	public static void main(String[] args) {
+
+		int a = 35;
+		int b = 25;
+		int c = 5;
+		
+		int Sum = a + b;
+		int Sub = Sum / c;
+		System.out.println(Sum);
+		System.out.println(Sub);
+		
+	}
+}
